26b8d30c8385b8cf4bbf125cea8b6b5d72b40a29
scastil <socastillogi@unal.edu.co>
2019-07-05 12:06:02 -0500

    cambio pequeno de actualizar cron al final de humedad.py

--- cprsora/humedad.py
+++ cprsora/humedad.py
@@ -296,19 +296,33 @@ class Humedad(cprv1.SqlDb):
                                      path_fuentes,colors,window,bottomtext,ylocfactor_texts,yloc_legends,
                                      title=title)
 ### cron
-def plot_HNetwork(nivel):
-    queryH=nivel.read_sql('select codigo from estaciones_estaciones where clase="H"')
+def plot_HNetwork(selfH,codes2fix,depths2drop):
+    queryH=cprv1.SqlDb(**selfH.local_server).read_sql('select codigo from estaciones_estaciones where clase="H" and estado in ("A","P")')
     codes=np.array(queryH['codigo'])
     ruta_figs='/media/nicolas/Home/Jupyter/Soraya/Op_Alarmas/Result_to_web/figs_operacionales/'
-    for code in codes:
+    
+    #drop depth list
+    drop_depths_list = [[None] for code in range(codes.size)]
+    for code2fix,depth in zip(codes2fix,depths2drop):
+        ind = np.where(codes==code2fix)[0][0]
+        drop_depths_list[ind] = depth
+    
+    for code,drop_depth in zip(codes,drop_depths_list):
         self= hm.Humedad(codigo=code)
         end = dt.datetime.now()
-        end = pd.to_datetime(end.strftime('%Y-%m-%d %H:%M'))
-        starts  = [(end - pd.Timedelta(hours=3)), (end - pd.Timedelta(hours=24)),
-                   (end - pd.Timedelta(hours=72)),(end - pd.Timedelta(days=30)) ]
+        starts  = [(end - dt.timedelta(hours=3)), (end - dt.timedelta(hours=24)),
+                   (end - dt.timedelta(hours=72)),(end - dt.timedelta(days=30)) ]
         for start in starts:
             #consulta pluvio
-            pluvio_s = cprv1.SqlDb(**self.remote_server1).read_sql("select p1/1000,p2/1000 from datos where cliente='%s' and (((fecha>'%s') or (fecha='%s' and hora>='%s')) and ((fecha<'%s') or (fecha='%s' and hora<='%s')))"%(int(self.info.get('pluvios')),start.strftime('%Y-%m-%d'),start.strftime('%Y-%m-%d'),start.strftime('%H:%M:%S'),end.strftime('%Y-%m-%d'),end.strftime('%Y-%m-%d'),end.strftime('%H:%M:%S')))
-            pluvio_s.columns= ['p1','p2']
-            self.plot_Humedad2Webpage(start,end,pluvio_s,ruta_figs)
+            P = cprv1.SqlDb(**self.remote_server1).read_sql("select fecha,hora,p1/1000,p2/1000 from datos where cliente='%s' and (((fecha>'%s') or (fecha='%s' and hora>='%s')) and ((fecha<'%s') or (fecha='%s' and hora<='%s')))"%(int(self.info.get('pluvios')),start.strftime('%Y-%m-%d'),start.strftime('%Y-%m-%d'),start.strftime('%H:%M:%S'),end.strftime('%Y-%m-%d'),end.strftime('%Y-%m-%d'),end.strftime('%H:%M:%S')))
+            P.columns=['fecha','hora','p1','p2']
+            nulls = np.where(P[['fecha']]['fecha'].isnull() == True)[0]
+            P= P.drop(nulls)
+            dates = [P['fecha'][i].strftime('%Y-%m-%d') +' '+str(P['hora'][i]).split(' ')[-1][:-3] for i in P.index]
+            P.index = pd.to_datetime(dates)
+            P['fecha_hora']= dates
+            P = P.drop_duplicates(['fecha_hora'],keep='first').asfreq('1T')
+            P = P.drop(['fecha','hora','fecha_hora'],axis=1)
+
+            self.plot_Humedad2Webpage(start,end,P,ruta_figs=ruta_figs,drop_depth=drop_depth)
     print 'Se ejecutan las graficas operacionales de la red de humedad'
